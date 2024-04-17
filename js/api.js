0ffe145f9f3fd7d7edb470d1ef78fccd6aee57b4
Ayush Saxena <ayushsaxena1405@gmail.com>
2024-04-17 16:16:47 +0530

    Initial Commit

---
+++ js/api.js
@@ -0,0 +1,21 @@
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+//const API_KEY = '0b4b0a20-3a07-4b1d-94c0-2e5c7cc274f2';
+// const API_KEY = '7410ce8b-0328-4303-8101-738a5ff652da';
+ const API_KEY = '58738d6b-1eb9-42e1-845b-78e59aeaf157';
+// const API_KEY = '6bea69d6-2b50-4f12-919a-c5a3a0a56afb';
+// const API_KEY = '8006972c-737d-473e-bd65-d6c2ce3a5ef7';
+// const API_KEY = 'ee6fbca9-9ba6-46c8-85db-80a20b23313d';
+// const API_KEY = '28767f45-f133-447e-9873-69d24bfb94ee';
+// const API_KEY = '413cd5e5-f20e-4811-8fd4-e606933497d7';
+// const API_KEY = '87953899-6cab-4cb0-82a8-cc100303d4c7';
+//const API_KEY = '788a453e-03d8-4066-86a2-6487ae995bae';
+ //const API_KEY = 'c823b640-11f7-4019-9185-0cc03ba0ca05';
+// const API_KEY = '120d39d1-3ec7-49b1-9e91-8e2baf0c6401';
+//const API_KEY = '8aee4a44-3640-4d8f-9796-7c7cda3c52ac';
+// const API_KEY = '14458836-cdfa-4545-9fe5-0c0765ab2bc2';
+ //const API_KEY = '2ad5cd99-f67e-40e1-b844-c9673ccc41bb';
+// const API_KEY = '6ab09e1e-be47-4a21-b026-e87781c88fce';
+// const API_KEY = '79e609cc-5129-4efa-974b-50b7b514cdd6';
+// const API_KEY = '8072aa49-c0fc-4b7d-acdb-39150f685e63';
